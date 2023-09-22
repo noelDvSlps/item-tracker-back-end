dee23b006a2070bb386a2fceb4057d9d17775c82
Noel Pulido <noelpulido1229@gmail.com>
2023-09-21 20:29:22 -0700

    remove duplicate files and edit database_url

--- src/helper.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.parseable = void 0;
-var zod_1 = require("zod");
-exports.parseable = zod_1.z
-    .string()
-    .transform(function (n) { return parseInt(n); })
-    .pipe(zod_1.z.number({ errorMap: function (_err) { return ({ message: "not parseable to a number" }); } }));
